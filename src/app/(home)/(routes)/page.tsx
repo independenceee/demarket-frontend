b715d3e2c6644dd84ca8a9c385b91ac518554593
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2023-10-25 17:27:11 +0700

    feat: fix background homepage

--- src/app/(home)/(routes)/page.tsx
+++ src/app/(home)/(routes)/page.tsx
@@ -1,9 +1,16 @@
+"use client";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 import React from "react";
+import { Autoplay, Navigation, Pagination, EffectCoverflow } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import Image from "next/image";
 import classNames from "classnames/bind";
 import Statistics from "@/components/Statistics";
-import styles from "./Home.module.scss";
 import images from "@/assets/images";
+import styles from "./Home.module.scss";
 type Props = {};
 
 const cx = classNames.bind(styles);
@@ -12,7 +19,86 @@ const Home = function ({}: Props) {
     return (
         <main className={cx("wrapper")}>
             <div className={cx("container")}>
-                <section className={cx("backgound")}></section>
+                <section className={cx("backgound__wrapper")}>
+                    <div className={cx("background__content")}>
+                        <h3 className={cx("background__content-title")}>Demarket</h3>
+                        <h3 className={cx("background__content-slug")}>
+                            Examples For Creating Solid UX Design
+                        </h3>
+                        <p className={cx("background__content-description")}>
+                            The title "The Power of the Ballot: Contributing to Democracy"
+                            captures the essence of the crucial role voting plays in
+                            empowering democratic systems. It highlights the significance
+                            of individual voices and their impact through the act of
+                            casting a ballot. The title combines the political aspect with
+                            a profound understanding of the power of democracy,
+                            emphasizing the influence citizens hold in shaping the course
+                            of their nations through the act of voting.
+                        </p>
+
+                        <div className={cx("button__wrapper")}>
+                            <button className={cx("button")}>Explore</button>
+                            <button className={cx("button")}>Getting Started</button>
+                        </div>
+                    </div>
+                    <div className={cx("background__slider")}>
+                        <Swiper
+                            effect={"coverflow"}
+                            grabCursor={true}
+                            centeredSlides={true}
+                            slidesPerView={"auto"}
+                            loop={true}
+                            autoplay={{
+                                delay: 2500,
+                                pauseOnMouseEnter: true,
+                                reverseDirection: true,
+                                disableOnInteraction: true,
+                            }}
+                            coverflowEffect={{
+                                rotate: 0,
+                                stretch: 0,
+                                depth: 200,
+                                modifier: 3,
+                            }}
+                            pagination={{ el: ".swiper-pagination", clickable: true }}
+                            navigation={{
+                                nextEl: ".swiper-button-next",
+                                prevEl: ".swiper-button-prev",
+                                hideOnClick: true,
+                            }}
+                            modules={[Autoplay, EffectCoverflow, Pagination, Navigation]}
+                            className={cx("background__slider-container")}
+                        >
+                            <SwiperSlide className={cx("background__slider-image")}>
+                                <Image
+                                    className={cx("image")}
+                                    src={images.background}
+                                    alt="slide_image"
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide className={cx("background__slider-image")}>
+                                <Image
+                                    className={cx("image")}
+                                    src={images.background}
+                                    alt="slide_image"
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide className={cx("background__slider-image")}>
+                                <Image
+                                    className={cx("image")}
+                                    src={images.background}
+                                    alt="slide_image"
+                                />
+                            </SwiperSlide>
+
+                            <div className="slider-controler">
+                                {/* <div className="swiper-button-prev slider-arrow"></div>
+                                <div className="swiper-button-next slider-arrow"></div> */}
+                                <div className="swiper-pagination"></div>
+                            </div>
+                        </Swiper>
+                    </div>
+                </section>
                 {/*  */}
                 <section className={cx("news__wrapper")}>
                     <header className={cx("header")}>
@@ -23,9 +109,7 @@ const Home = function ({}: Props) {
                         </p>
                     </header>
 
-                    <div className={cx("news__container")}>
-                        
-                    </div>
+                    <div className={cx("news__container")}></div>
                 </section>
                 <section className={cx("trending__wrapper")}>
                     <header className={cx("header")}>
