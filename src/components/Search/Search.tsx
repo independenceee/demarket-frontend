e186d59cc25334031264a65114ad3a7facbc3429
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-01-12 10:13:28 +0700

    fix: search param router mapping url

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,54 +1,76 @@
 "use client";
 
-import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+import React, { ChangeEvent, useEffect, useRef, useState, useCallback, memo } from "react";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import classNames from "classnames/bind";
+import { QueryParamsType } from "@/types/GenericsType";
 import { useDebounce } from "@/hooks";
 import { SearchIcon } from "@/components/Icons";
 import styles from "./Search.module.scss";
+import { useQueryState } from "nuqs";
 
 const cx = classNames.bind(styles);
 type Props = {
-    searchValue: string;
-    setSearchValue: React.Dispatch<React.SetStateAction<string>>;
+    searchValueParam: string;
+    setSearchValueParam: React.Dispatch<React.SetStateAction<string>>;
 };
 
-const Search = function ({ searchValue, setSearchValue }: Props) {
+const Search = function ({ searchValueParam, setSearchValueParam }: Props) {
+    const router = useRouter();
+    const pathname: string = usePathname();
+    const searchParams = useSearchParams();
+
+    const [searchValueQuery, setSearchValueQuery] = useQueryState<QueryParamsType>("search", {
+        defaultValue: { search: searchValueParam as string },
+        parse: (query) => JSON.parse(query) as QueryParamsType,
+    });
+
     const [loading, setLoading] = useState<boolean>(false);
-
+    const debouncedValue = useDebounce(searchValueParam, 500);
     const inputRef = useRef<HTMLInputElement>(null!);
-    const debouncedValue = useDebounce(searchValue, 500);
 
-    useEffect(
-        function () {
-            if (!debouncedValue.trim()) {
-                return;
+    useEffect(() => {
+        if (!debouncedValue.trim()) {
+            return;
+        }
+        const handleSearchData = function () {
+            try {
+                setLoading(true);
+            } catch (error) {
+                setLoading(false);
             }
-            const handleSearchData = function () {
-                try {
-                    setLoading(true);
-                } catch (error) {
-                    setLoading(false);
-                }
-            };
+        };
 
-            handleSearchData();
-        },
-        [debouncedValue],
-    );
+        handleSearchData();
+    }, [debouncedValue]);
 
     const handleClear = function () {
-        setSearchValue("");
+        setSearchValueParam("");
         inputRef.current.focus();
     };
 
-    const handleChange = function (event: ChangeEvent<HTMLInputElement>) {
+    const handleChange = useCallback(function (event: ChangeEvent<HTMLInputElement>) {
         const searchValue = event.target.value;
         if (!searchValue.startsWith(" ")) {
-            setSearchValue(searchValue);
+            setSearchValueParam(searchValue);
+            setSearchValueQuery({ search: searchValue } as QueryParamsType);
         }
-    };
+    }, []);
+
+    useEffect(() => {
+        if (searchValueQuery) {
+            const { search } = searchValueQuery;
+            setSearchValueParam(search as string);
+        }
+    }, [searchValueQuery]);
+
+    useEffect(() => {
+        const params = new URLSearchParams(searchParams);
+        params.set("search", searchValueParam);
+        router.replace(pathname + "?" + params.toString(), { scroll: false });
+    }, [router, searchValueQuery]);
 
     return (
         <section className={cx("wrapper")}>
@@ -56,13 +78,13 @@ const Search = function ({ searchValue, setSearchValue }: Props) {
             <article className={cx("container")}>
                 <input
                     ref={inputRef}
-                    value={searchValue}
+                    value={searchValueParam}
                     spellCheck={false}
                     type="text"
                     onChange={handleChange}
                     placeholder="Search accounts and assets ..."
                 />
-                {!!searchValue && !loading && (
+                {!!searchValueParam && !loading && (
                     <button className={cx("clear")} onClick={handleClear}>
                         <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
